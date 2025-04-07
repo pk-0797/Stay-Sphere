c2df5cc146904c39faa8dbe0588460737199dbd5
Prahlad Kumar <prahladkumar1401@gmail.com>
2025-04-07 08:05:17 +0530

    Error404

--- src/component/common/Error404.jsx
+++ src/component/common/Error404.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 export const Error404 = () => {
   return (
     <div>
-        <h1 style={{textAlign:"center",color:"rgb(192, 112, 8)",fontSize:"50px",position:"absolute",top:"50%",left:"45%"}}>Error Page..</h1>
+      <h1
+        style={{
+          textAlign: "center",
+          color: "rgb(192, 112, 8)",
+          fontSize: "50px",
+          position: "absolute",
+          top: "50%",
+          left: "45%",
+        }}
+      >
+        Error Page..
+      </h1>
     </div>
-  )
-}
+  );
+};
