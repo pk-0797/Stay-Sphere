b292b9aab2c761dceb641de81bc9dd5cb537a95c
Prahlad Kumar <prahladkumar1401@gmail.com>
2025-08-11 11:25:07 +0530

    baseurl in aap.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ import ReportAdmin from "./component/user/ReportAdmin";
 // className= "app-wrapper" in UserSidebar and HostSidebar
 
 function App() {
-  axios.defaults.baseURL = "http://localhost:3002";
+  axios.defaults.baseURL = "https://stay-sphere-gray.vercel.app";
 
   return (
     <>
