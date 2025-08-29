1f7d315dfdc6da0d0baf7850d16c205cd4e22316
Prahlad Kumar <prahladkumar1401@gmail.com>
2025-08-29 12:35:59 +0530

    Host and User Responsive

--- src/App.jsx
+++ src/App.jsx
@@ -53,96 +53,86 @@ function App() {
 
   return (
     <>
-      <body className="layout-fixed sidebar-expand-lg bg-body-tertiary app-loaded sidebar-open">
-        <div className="app-wrapper app-main">
-          <Routes>
-            <Route path="/admin/login" element={<AdminLogin />}></Route>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/aboutus" element={<AboutUs />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/signup" element={<SignUp />}></Route>
-            <Route path="/forgotpassword" element={<ForgetPass />}></Route>
+      <Routes>
+        <Route path="/admin/login" element={<AdminLogin />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/aboutus" element={<AboutUs />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/forgotpassword" element={<ForgetPass />}></Route>
 
+        <Route path="/resetpassword/:token" element={<ResetPassword />}></Route>
+        <Route path="/property/details/:id" element={<PropertyDetails />} />
+
+        {/* For User */}
+        <Route element={<PrivateRoute allowedRoles={["user"]} />}>
+          <Route path="/user" element={<UserSidebar />}>
+            <Route path="profile" element={<UserProfile />}></Route>
+            <Route path="wishlist" element={<Wishlist />}></Route>
+            <Route path="booking/:id" element={<AddBooking />}></Route>
             <Route
-              path="/resetpassword/:token"
-              element={<ResetPassword />}
+              path="property/details/:id"
+              element={<HomeDetails />}
             ></Route>
-            <Route path="/property/details/:id" element={<PropertyDetails />} />
+            <Route path="mybooking" element={<ViewMyBooking />}></Route>
+            <Route path="home" element={<UserHome />}></Route>
+            <Route
+              path="messages"
+              element={<MessageComponent></MessageComponent>}
+            ></Route>
+            <Route path="bookingreview" element={<Review />}></Route>
+            <Route
+              path="contact-host/:bookingId/:hostId"
+              element={<ContactHostForm />}
+            />
+            <Route
+              path="bookingreview/reviewform/:propertyId/:bookingId"
+              element={<ReviewFormForUser />}
+            />
+            <Route path="notifications" element={<Notifications />}></Route>
+            <Route path="contact-admin" element={<ReportAdmin />} />
+          </Route>
+        </Route>
 
-            {/* For User */}
-            <Route element={<PrivateRoute allowedRoles={["user"]} />}>
-              <Route path="/user" element={<UserSidebar />}>
-                <Route path="profile" element={<UserProfile />}></Route>
-                <Route path="wishlist" element={<Wishlist />}></Route>
-                <Route path="booking/:id" element={<AddBooking />}></Route>
-                <Route
-                  path="property/details/:id"
-                  element={<HomeDetails />}
-                ></Route>
-                <Route path="mybooking" element={<ViewMyBooking />}></Route>
-                <Route path="home" element={<UserHome />}></Route>
-                <Route
-                  path="messages"
-                  element={<MessageComponent></MessageComponent>}
-                ></Route>
-                <Route path="bookingreview" element={<Review />}></Route>
-                <Route
-                  path="contact-host/:bookingId/:hostId"
-                  element={<ContactHostForm />}
-                />
-                <Route
-                  path="bookingreview/reviewform/:propertyId/:bookingId"
-                  element={<ReviewFormForUser />}
-                />
-                <Route path="notifications" element={<Notifications />}></Route>
-                <Route path="contact-admin" element={<ReportAdmin />} />
-              </Route>
-            </Route>
+        {/* For Host */}
+        <Route element={<PrivateRoute allowedRoles={["host"]} />}>
+          <Route path="/host" element={<HostSidebar />}>
+            <Route path="home" element={<HostHome />} />
+            <Route path="profile" element={<HostProfile />} />
+            <Route path="messages" element={<MessagesFromUsers />}></Route>
+            <Route
+              path="/host/updateproperty/:id"
+              element={<UpdatePropertyPrice />}
+            />
 
-            {/* For Host */}
-            <Route element={<PrivateRoute allowedRoles={["host"]} />}>
-              <Route path="/host" element={<HostSidebar />}>
-                <Route path="home" element={<HostHome />} />
-                <Route path="profile" element={<HostProfile />} />
-                <Route path="messages" element={<MessagesFromUsers />}></Route>
-                <Route
-                  path="/host/updateproperty/:id"
-                  element={<UpdatePropertyPrice />}
-                />
+            <Route path="addproperty" element={<AddProperty />} />
+            <Route path="editproperty/:id" element={<AddProperty />} />
+            <Route path="myproperty" element={<ViewMyProperty />} />
+            <Route
+              path="bookingrequest"
+              element={<UpdateOrCancelBooking></UpdateOrCancelBooking>}
+            ></Route>
+            <Route path="contact-admin" element={<AdminReport />} />
+          </Route>
+        </Route>
 
-                <Route path="addproperty" element={<AddProperty />} />
-                <Route path="editproperty/:id" element={<AddProperty />} />
-                <Route path="myproperty" element={<ViewMyProperty />} />
-                <Route
-                  path="bookingrequest"
-                  element={<UpdateOrCancelBooking></UpdateOrCancelBooking>}
-                ></Route>
-                <Route path="contact-admin" element={<AdminReport />} />
-              </Route>
-            </Route>
-
-            {/* For Admin */}
-            <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
-              <Route path="/admin" element={<AdminSidebar />}>
-                <Route path="home" element={<AdminHome />} />
-                <Route path="profile" element={<AdminProfile />} />
-                <Route path="dashboard" element={<AdminDashboard />} />
-                <Route
-                  path="allusers"
-                  element={<ManageUsers></ManageUsers>}
-                ></Route>
-                <Route
-                  path="allproperties"
-                  element={<ManageProperties />}
-                ></Route>
-                <Route path="allbookings" element={<ManageBookings />}></Route>
-                <Route path="allreports" element={<UserMessages />} />
-              </Route>
-            </Route>
-            <Route path="/*" element={<Error404 />}></Route>
-          </Routes>
-        </div>
-      </body>
+        {/* For Admin */}
+        <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+          <Route path="/admin" element={<AdminSidebar />}>
+            <Route path="home" element={<AdminHome />} />
+            <Route path="profile" element={<AdminProfile />} />
+            <Route path="dashboard" element={<AdminDashboard />} />
+            <Route
+              path="allusers"
+              element={<ManageUsers></ManageUsers>}
+            ></Route>
+            <Route path="allproperties" element={<ManageProperties />}></Route>
+            <Route path="allbookings" element={<ManageBookings />}></Route>
+            <Route path="allreports" element={<UserMessages />} />
+          </Route>
+        </Route>
+        <Route path="/*" element={<Error404 />}></Route>
+      </Routes>
     </>
   );
 }
